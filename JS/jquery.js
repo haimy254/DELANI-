2b7831af40f740da1b4f5b83d677c09ee34ced70
haimy254 <haimanaare@gmail.com>
2022-03-12 19:31:35 +0300

    adding designs

--- JS/jquery.js
+++ JS/jquery.js
@@ -3,3 +3,8 @@ $(document).ready(function () {
     $("p.item-text-1").toggle();
   });
 });
+$(document).ready(function () {
+  $("button").click(function () {
+    $("p.item-text2").toggle();
+  });
+});

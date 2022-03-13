227d5e819f9ba0d944c78d6dafd8566d5cdcf196
haimy254 <haimanaare@gmail.com>
2022-03-13 11:55:35 +0300

    toggled

--- JS/jquery.js
+++ JS/jquery.js
@@ -1,17 +1,22 @@
 $(document).ready(function () {
-  $("button").click(function () {
-    $("p.item-text1").toggle();
+  $(".itemss").click(function () {
+    $(".itemss").hide();
+    $(".item-text2").show();
+  });
+  $(".item-text2").click(function () {
+    $(".item-text2").toggle();
+    $(".itemss").toggle();
   });
 });
 
-$(document).ready(function () {
-  $("button").click(function () {
-    $("#item-text2").toggle();
-  });
-});
+// $(document).ready(function () {
+//   $("button").click(function () {
+//     $("#item-text2").toggle();
+//   });
+// });
 
-$(document).ready(function () {
-  $("button").click(function () {
-    $("p.item-text3").toggle();
-  });
-});
+// $(document).ready(function () {
+//   $("button").click(function () {
+//     $("p.item-text3").toggle();
+//   });
+// });

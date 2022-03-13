9a3898097ce496cf5a41a358d459fb0ca4226f31
haimy254 <haimanaare@gmail.com>
2022-03-14 00:31:25 +0300

    message

---
+++ JS/index.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+  $("#my-form").submit(function (event) {
+    event.preventDefault();
+
+    let name = $("#name").val();
+    let email = $("#email").val();
+    let message = $("#message").val();
+
+    sendEmail(name, email, message);
+
+    $(".message").addClass("show");
+    alert("message submitted");
+
+    function sendEmail(name, email, message) {
+      email.send({
+        Host: "smt.gmail.com",
+        Username: "haimanaare@gmail.com",
+        Password: "eawbopgsanmmcwsq",
+        To: "",
+      });
+    }
+  });
+});

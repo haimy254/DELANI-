648e1f1c36ddba0b471ef8385905502532e88411
haimy254 <haimanaare@gmail.com>
2022-03-13 19:05:10 +0300

    corrected jquery

--- JS/jquery.js
+++ JS/jquery.js
@@ -7,6 +7,22 @@ $(document).ready(function () {
     $(".item-text2").toggle();
     $(".itemss").toggle();
   });
+  $(".itemss2").click(function () {
+    $(".itemss2").hide();
+    $(".item-text3").show();
+  });
+  $(".item-text3").click(function () {
+    $(".item-text3").toggle();
+    $(".itemss2").toggle();
+  });
+  $(".itemss1").click(function () {
+    $(".itemss1").hide();
+    $(".item-text1").show();
+  });
+  $(".item-text1").click(function () {
+    $(".item-text1").toggle();
+    $(".itemss1").toggle();
+  });
 });
 
 // $(document).ready(function () {
